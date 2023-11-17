d6cd4d5b6b0da1c67c6f0d83257b3957878b858a
Sebastian Deisel <sebastian.deisel@gmail.com>
2023-11-17 16:08:00 +0100

    PWA manifest and service worker

--- ui/webapp/vite.config.ts
+++ ui/webapp/vite.config.ts
@@ -2,13 +2,47 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import { VitePWA } from 'vite-plugin-pwa'
 
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), VueI18nPlugin({})],
+  plugins: [
+    vue(),
+    vueJsx(),
+    VueI18nPlugin({}),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: [
+        'android-chrome-192x192.png',
+        'android-chrome-512x512.png',
+        'apple-touch-icon.png',
+        'favicon.ico',
+        'safari-pinned-tab.svg'
+      ],
+      manifest: {
+        name: 'Pic2Plate',
+        short_name: 'Pic2Plate',
+        description: 'Pic2Plate - Recipe finder',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
