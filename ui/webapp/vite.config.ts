ead0b7aa7336fd846d540d6bbd542b7a99e8781d
Sebastian Deisel <sebastian.deisel@gmail.com>
2023-11-19 10:57:30 +0100

    Update vite.config.ts

--- ui/webapp/vite.config.ts
+++ ui/webapp/vite.config.ts
@@ -15,9 +15,6 @@ export default defineConfig({
     VueI18nPlugin({}),
     VitePWA({
       registerType: 'autoUpdate',
-      devOptions: {
-        enabled: true
-      },
       includeAssets: [
         'android-chrome-192x192.png',
         'android-chrome-512x512.png',
