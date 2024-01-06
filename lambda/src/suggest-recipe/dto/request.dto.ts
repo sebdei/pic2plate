8496c4225e2821c2339ee55fa08813305d56b6c9
Alexander Euler <alexandereuler@t-online.de>
2024-01-06 22:36:33 +0100

    fix

--- lambda/src/suggest-recipe/dto/request.dto.ts
+++ lambda/src/suggest-recipe/dto/request.dto.ts
@@ -1,21 +1,5 @@
-import { IsOptional, IsString } from 'class-validator'
-
 export interface RecipeRequestDto {
   image_data_url: string | null
   ingredients: string[] | null
   history: string[] | null
-}
-
-export class RecipeRequest implements RecipeRequestDto {
-  @IsOptional()
-  @IsString()
-  public image_data_url: string | null
-
-  @IsOptional()
-  @IsString()
-  public ingredients: string[] | null
-
-  @IsOptional()
-  @IsString({ each: true })
-  public history: string[] | null
 }
